b4eb0f57b0a282339ecc9edde669831875f4ecba
Leo2306 <leonardonarvaja232@gmail.com>
2021-11-11 23:06:23 -0300

    Actualizacion de direcciones archivos de estilos, header responsive a las paginas, cambio de nombre de clase para la navegacion, estructura de paginas registro inicio sesion y agregado de carrusel al blog.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const menuBtn = document.querySelector(".menu-toggle");
-const topNav = document.querySelector(".navbar");
+const topNav = document.querySelector(".navbar-style");
 if(menuBtn && topNav){
 menuBtn.addEventListener("click", function () {
-    topNav.classList.toggle("navbar-open");
+    topNav.classList.toggle("navbar-style-open");
     menuBtn.classList.toggle("menu-open");
 });
 }
