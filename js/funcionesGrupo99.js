6f2d6a0e9da897fc2c26e7a4907c7086d6d2f40e
alanrios21 <riosalan264@gmail.com>
2021-11-14 00:26:14 -0300

    Cambio de imagenes y puntos 1,2,3 y 5 de js hechos

---
+++ js/funcionesGrupo99.js
@@ -0,0 +1,63 @@
+const menuBtn = document.querySelector(".menu-toggle");
+const topNav = document.querySelector(".navbar-style");
+if(menuBtn && topNav){
+menuBtn.addEventListener("click", function () {
+    topNav.classList.toggle("navbar-style-open");
+    menuBtn.classList.toggle("menu-open");
+});
+}
+
+function ageCalculator() {  
+  var userinput = document.getElementById("date").value;  
+  var dob = new Date(userinput);  
+  if(userinput==null || userinput=='') {      
+    return false;   
+  } 
+  
+  else {  
+    
+  //calculate month difference from current date in time  
+  var month_diff = Date.now() - dob.getTime();  
+    
+  //convert the calculated difference in date format  
+  var age_dt = new Date(month_diff);   
+    
+  //extract year from date      
+  var year = age_dt.getUTCFullYear();  
+    
+  //now calculate the age of the user  
+  var age = Math.abs(year - 1970);  
+    
+  //display the calculated age  
+ return alert(age)
+  } 
+  
+  
+}  
+
+function validar() {
+   var txtNombre = document.getElementById('nombre'); 
+   var txtApellido = document.getElementById('apellido'); 
+
+   if (txtNombre.value.length <= '2') {
+      alert('El nombre debe tener mas de 2 caracteres');
+      return false;
+      
+  } 
+
+
+    if (txtApellido.value.length <= "1") {
+      alert('El apellido debe tener mas de 1 caracter ');
+      return false;
+  }  
+   
+  }
+  
+  function mostrarAlertOperacion(){
+    alert('Operacion Exitosa');
+  }
+
+  const boton = document.getElementById("btn"); boton.addEventListener("click", ()=>{ boton.style.color = "white"; boton.style.backgroundColor = "green"; });
+
+    
+  
--- js/script.js
+++
@@ -1,9 +0,0 @@
-const menuBtn = document.querySelector(".menu-toggle");
-const topNav = document.querySelector(".navbar-style");
-if(menuBtn && topNav){
-menuBtn.addEventListener("click", function () {
-    topNav.classList.toggle("navbar-style-open");
-    menuBtn.classList.toggle("menu-open");
-});
-}
-
